cccc9ff7dcf61f54427e90cb75a090c79c15bcea
Gaellga <gaellegaban@gmail.com>
2019-06-18 12:36:51 +0200

    cover

--- accueil.php
+++ accueil.php
@@ -1,8 +1,37 @@
-<section>
-    <h1 class="jumbotron">Ceci est la page d'accueil</h1>
-        <div class="container pb-4">
-            <div class="row">
-                <div class="col-12 col-md-12 text-center">
+<header>
+    <div id="containerCouv">
+        <img src="assets/img/couv8-1.jpg" alt="Retour au menu" width="700"/>
+    </div>
+</header>
+<nav class="navbar navbar-expand-md navbar-light">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="true" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse flex-row-center" id="navbarToggler">
+            <ul class="nav navbar-nav nav-pills">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Accueil
+                        <span class="sr-only">(page actuelle)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="lecture.php">Lire</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="apropos.php">À propos</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="d-inline float-right ">
+        <a class="langue" href="#">FR</a><span id="espace">|</span><a class="langue" href="#">ENG</a>
+    </div>
+</nav>
+<section id="accueil">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-12 col-md-12 text-center">
                 <p>Hard cheese ricotta queso. Cut the cheese queso goat caerphilly cheeseburger caerphilly pepper jack boursin. Lancashire pecorino parmesan swiss bavarian bergkase manchego boursin macaroni cheese. Dolcelatte cottage cheese cheese slices goat brie everyone loves cream cheese.
 
 Squirty cheese feta everyone loves. The big cheese chalk and cheese cauliflower cheese cream cheese stinking bishop babybel cheese on toast cheese strings. Cheese slices cheesecake stinking bishop roquefort queso pepper jack cheese slices danish fontina. Fromage melted cheese mascarpone blue castello cauliflower cheese.
@@ -12,7 +41,7 @@ Paneer parmesan queso. Monterey jack camembert de normandie everyone loves cream
 Queso caerphilly danish fontina. Cottage cheese cheesy feet croque monsieur danish fontina danish fontina stinking bishop cheese strings stilton. Roquefort edam edam cheese slices croque monsieur monterey jack squirty cheese when the cheese comes out everybody's happy. Taleggio cheesy feet goat everyone loves port-salut caerphilly bocconcini queso. Halloumi roquefort.
 
 Cheese strings monterey jack squirty cheese. Manchego cheese slices cut the cheese swiss camembert de normandie cheese and biscuits boursin cheesy grin. Say cheese cheese and wine bocconcini squirty cheese emmental pecorino red leicester lancashire. Fromage frais melted cheese cheesy grin chalk and cheese fromage mascarpone.</p>
-                </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    require "head.php";
+    //require "header.php";
+    require "accueil.php";
+    require "footer.php";
+?>
