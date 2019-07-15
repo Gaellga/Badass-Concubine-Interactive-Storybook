ff054eed92c3fce085da5becd8c0a238ff035a71
Gaellga <gaellegaban@gmail.com>
2019-07-15 16:48:33 +0200

    pages index

--- lecture.php
+++ lecture.php
@@ -4,39 +4,38 @@
 <nav class="navbar navbar-expand-md navbar-light">
     <div class="container-fluid">
         <div class="collapse navbar-collapse flex-row" id="navbarToggler">
-        <a href="index.php" aria-label="Return to main page"><img src="assets/img/home-mini-1.jpg" alt="Retour au menu"/></a>
-            </div>
+            <a href="index.php" aria-label="Return to main page"><img src="assets/img/home-mini-1.jpg"
+                    alt="Retour au menu" /></a>
         </div>
-    </nav>
+    </div>
+</nav>
 <section>
     <div class="containerPages">
         <div class="row" id="pageSize">
             <div class="col-12 text-center">
-                <img src="assets/img/ch1/wu_ep1_00003.jpg" alt="Page" class="no-resize">
+                <img src="assets/img/ch1/wu_ep1_15.jpg" alt="Page" class="no-resize">
             </div>
         </div>
     </div>
-    <div id ="flecheBas" class="row p-4">
-            <div class="col-12 text-center">
-                <a href="#" onclick="showDiv()"><i class="fas fa-chevron-up"></i></a>
-            </div>
+    <div id="flecheBas" class="row">
+        <div class="col-12 text-center">
+            <a href="#" onclick="showDiv()"><i class="fas fa-chevron-up"></i></a>
         </div>
+    </div>
     <div id="hiddenDiv" style="display: none;">
         <div class="row pt-3">
             <div class="col-12 text-center">
                 <a href="#" onclick="hideDiv()"><i class="fas fa-chevron-down"></i></a>
             </div>
         </div>
+
         <div class="carousel">
             <div class="carousel-row">
-                <div class="carousel-tile"><img src="assets/img/ch1/carte_0_taizong.jpg" alt="Page précedente" class="previousNext"></div>
-                <div class="carousel-tile"><img src="assets/img/ch1/wu_ep1_00003.jpg" alt="Page suivante 1" class="previousNext"></div>
-                <div class="carousel-tile"><img src="assets/img/ch1/wu_ep1_00005.jpg" alt="Page suivante 2" class="previousNext"></div>
-                <div class="carousel-tile"><img src="assets/img/ch1/wu_ep1_00006.jpg" alt="Page suivante 1" class="previousNext"></div>
-                <div class="carousel-tile"><img src="assets/img/ch1/wu_ep1_00007.jpg" alt="Page suivante 2" class="previousNext"></div>
-                <div class="carousel-tile"><img src="assets/img/ch1/wu_ep1_00008.jpg" alt="Page suivante 2" class="previousNext"></div>
-                <div class="carousel-tile"><img src="assets/img/ch1/wu_ep1_00011.jpg" alt="Page suivante 1" class="previousNext"></div>
-                <div class="carousel-tile"><img src="assets/img/ch1/wu_ep1_00012.jpg" alt="Page suivante 2" class="previousNext"></div>
+                <?php for ( $x = 1 ; $x < 83 ; $x++ ) { ?>
+                <div class="carousel-tile">
+                    <img src="assets/img/ch1/wu_ep1_<?=$x?>.jpg" alt="Page introuvable" class="previousNext" /></div>
+
+                <?php } ?>
             </div>
         </div>
 
